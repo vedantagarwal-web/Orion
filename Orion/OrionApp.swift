bef68ef6204600441cfeeb41f67636301286e22f
Vedant Agarwal <vedantagarwal453@gmail.com>
2024-08-24 02:07:57 -0700

    Initial Commit

---
+++ Orion/OrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrionApp.swift
+//  Orion
+//
+//  Created by Vedant Agarwal on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct OrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
